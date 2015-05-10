ab1e86c681055f2adf1c14d50939fa310e143dab
sakaios <oh11sam@gmail.com>
2015-05-10 23:21:21 +0900

    作成済みのTodoリソースを全件取得するAPI(GET Todos)の処理を、TodoRestControllerのgetTodosメソッドに実装

--- src/main/java/todo/api/todo/TodoRestController.java
+++ src/main/java/todo/api/todo/TodoRestController.java
@@ -1,10 +1,40 @@
 package todo.api.todo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.dozer.Mapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import todo.domain.model.Todo;
+import todo.domain.service.todo.TodoService;
 
 @Controller
-@RequestMapping("todos") // (1)
+@RequestMapping("todos")
 public class TodoRestController {
+    @Inject
+    TodoService todoService;
+    @Inject
+    Mapper beanMapper;
+
+    @RequestMapping(method = RequestMethod.GET) // (1)
+    @ResponseBody // (2)
+    @ResponseStatus(HttpStatus.OK) // (3)
+    public List<TodoResource> getTodos() {
+        Collection<Todo> todos = todoService.findAll();
+        List<TodoResource> todoResources = new ArrayList<>();
+        for (Todo todo : todos) {
+            todoResources.add(beanMapper.map(todo, TodoResource.class)); // (4)
+        }
+        return todoResources; // (5)
+    }
 
 }
