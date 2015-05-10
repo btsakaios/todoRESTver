d535489a68d3b81db10c3819557c3b351303c1bc
sakaios <oh11sam@gmail.com>
2015-05-10 23:40:40 +0900

    TodoServiceにfindOmeメソッドを実装

--- src/main/java/todo/domain/service/todo/TodoServiceImpl.java
+++ src/main/java/todo/domain/service/todo/TodoServiceImpl.java
@@ -16,32 +16,30 @@ import org.terasoluna.gfw.common.message.ResultMessages;
 import todo.domain.model.Todo;
 import todo.domain.repository.todo.TodoRepository;
 
-@Service// (1)
-@Transactional // (2)
+@Service
+@Transactional
 public class TodoServiceImpl implements TodoService {
+    @Inject
+    TodoRepository todoRepository;
 
     private static final long MAX_UNFINISHED_COUNT = 5;
 
-    @Inject// (3)
-    TodoRepository todoRepository;
-
-    // (4)
+    @Override
+    @Transactional(readOnly = true)
     public Todo findOne(String todoId) {
         Todo todo = todoRepository.findOne(todoId);
         if (todo == null) {
-            // (5)
             ResultMessages messages = ResultMessages.error();
             messages.add(ResultMessage
                     .fromText("[E404] The requested Todo is not found. (id="
                             + todoId + ")"));
-            // (6)
             throw new ResourceNotFoundException(messages);
         }
         return todo;
     }
 
     @Override
-    @Transactional(readOnly = true) // (7)
+    @Transactional(readOnly = true)
     public Collection<Todo> findAll() {
         return todoRepository.findAll();
     }
@@ -54,11 +52,10 @@ public class TodoServiceImpl implements TodoService {
             messages.add(ResultMessage
                     .fromText("[E001] The count of un-finished Todo must not be over "
                             + MAX_UNFINISHED_COUNT + "."));
-            // (8)
+
             throw new BusinessException(messages);
         }
 
-        // (9)
         String todoId = UUID.randomUUID().toString();
         Date createdAt = new Date();
 
