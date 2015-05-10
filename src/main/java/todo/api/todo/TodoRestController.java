4faf383dcc8b675f06cf44c0f40f49f426e8688d
sakaios <oh11sam@gmail.com>
2015-05-10 23:19:28 +0900

    TodoResourceのREST APIを提供するTodoRestControllerクラスを作成

---
+++ src/main/java/todo/api/todo/TodoRestController.java
@@ -0,0 +1,10 @@
+package todo.api.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("todos") // (1)
+public class TodoRestController {
+
+}
